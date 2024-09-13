f37a57521d8995f17a5a4554d102b76daf8c1eb5
reetu95 <reetuthimmaiah400@gmail.com>
2024-09-13 17:00:10 -0400

    requirements added

--- template.py
+++ template.py
@@ -21,7 +21,8 @@ list_of_files = [
     "prams.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
